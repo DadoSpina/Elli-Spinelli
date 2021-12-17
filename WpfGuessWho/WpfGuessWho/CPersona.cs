753d390129908f29b49f5f9b0943066d4c90ce49
elMatte <ellimatteo57@gmail.com>
2021-12-17 13:50:17 +0100

    Fetch1
    inizio divisione classi e sistemazioni protocollo

--- WpfGuessWho/WpfGuessWho/CPersona.cs
+++ WpfGuessWho/WpfGuessWho/CPersona.cs
@@ -8,5 +8,19 @@ namespace WpfGuessWho
 {
     class CPersona
     {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string capelli { get; set; }
+        public string occhi { get; set; }
+
+        public CPersona(int id, string nome, string capelli, string occhi)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.capelli = capelli;
+            this.occhi = occhi;
+        }
+
+
     }
 }
--- WpfGuessWho/WpfGuessWho/DatiCondivisi.cs
+++ WpfGuessWho/WpfGuessWho/DatiCondivisi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace WpfGuessWho
 {
     class DatiCondivisi
     {
+        UdpClient peer;
+        List<CPersona> listPersona = new List<CPersona>();
+
+        public void setPeer(string ip, int porta)
+        {
+            peer = new UdpClient(ip, porta);
+        }
     }
 }
