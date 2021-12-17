9d70857c049d907cdd9da69cfc207d5626828716
elMatte <ellimatteo57@gmail.com>
2021-12-17 12:40:51 +0100

    Prova
    Commento main

--- WpfGuessWho/WpfGuessWho/MainWindow.xaml.cs
+++ WpfGuessWho/WpfGuessWho/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,9 @@ namespace WpfGuessWho
         public MainWindow()
         {
             InitializeComponent();
+
+            /*Thread t = new Thread(ThreadServer.listen);
+            t.Start(c)*/
         }
     }
 }
